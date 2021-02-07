dcf149388149bb4dc44865960cb5a7523a38c030
Tobin Juday <tobinibot@gmail.com>
2021-02-07 16:55:37 -0500

    cleanup

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -2,24 +2,14 @@
 
 import {Tooltip, Popover} from 'bootstrap'
 
-document.addEventListener("turbolinks:load", () => {
-    console.log('Enabling Bootstrap tooltips and popovers…');
-
+function enableBootstrapFeatures() {
     const tooltips = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     const popovers = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
 
-    console.log(`Tooltips found: ${tooltips.length}, Popovers found: ${popovers.length}`);
+    console.log(`Bootstrap init: tooltips found: ${tooltips.length}, popovers found: ${popovers.length}`);
 
     tooltips.map(t => new Tooltip(t));
     popovers.map(p => new Popover(p));
-});
+}
 
-// console.log('setting up tooltips…')
-//
-// const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-//
-// console.log('')
-//
-// const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-//     return new bootstrap.Tooltip(tooltipTriggerEl)
-// });
+document.addEventListener("turbolinks:load", enableBootstrapFeatures);
