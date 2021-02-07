a286d59aadb854456e40aacb7d3d27dbb25421c5
Tobin Juday <tobinibot@gmail.com>
2021-02-07 13:03:56 -0500

    switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2021_01_31_171734) do
 
-  create_table "donations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "donations", force: :cascade do |t|
     t.date "date"
     t.bigint "donor_id", null: false
     t.string "method"
@@ -25,7 +28,7 @@ ActiveRecord::Schema.define(version: 2021_01_31_171734) do
     t.index ["donor_id"], name: "index_donations_on_donor_id"
   end
 
-  create_table "donors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "donors", force: :cascade do |t|
     t.string "name"
     t.string "address1"
     t.string "address2"
@@ -34,28 +37,6 @@ ActiveRecord::Schema.define(version: 2021_01_31_171734) do
     t.string "zip", limit: 10
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "original_donations", id: { type: :bigint, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.date "date", null: false
-    t.string "donor_full_name", limit: 128, null: false
-    t.string "method", limit: 16, null: false
-    t.string "account", limit: 24, null: false
-    t.decimal "amount", precision: 8, scale: 2
-    t.boolean "tax_deductible", default: false
-    t.string "memo"
-    t.index ["id"], name: "id", unique: true
-  end
-
-  create_table "original_donors", id: { type: :bigint, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "full_name", limit: 128
-    t.string "address"
-    t.string "address1", limit: 128
-    t.string "address2", limit: 128
-    t.string "city", limit: 64
-    t.string "state", limit: 2
-    t.string "zip", limit: 10
-    t.index ["id"], name: "id", unique: true
   end
 
   add_foreign_key "donations", "donors"
