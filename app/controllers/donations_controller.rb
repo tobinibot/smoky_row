fb6fe8ee5bd688ad6976cf376b7ff4eb85a78445
Tobin Juday <tobinibot@gmail.com>
2021-02-08 07:14:13 -0500

    add donation CRU

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -2,4 +2,40 @@ class DonationsController < ApplicationController
   def index
     @donations = Donation.all
   end
+
+  def new
+    @donation = Donation.new
+    @donors = Donor.all.order(:name).map{|d| [d.name, d.id]}
+  end
+
+  def create
+    @donation = Donation.new(donation_params)
+
+    if @donation.save
+      redirect_to donations_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @donation = Donation.find(params[:id])
+    @donors = Donor.all.order(:name).map{|d| [d.name, d.id]}
+  end
+
+  def update
+    @donation = Donation.find(params[:id])
+
+    if @donation.update(donation_params)
+      redirect_to donations_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def donation_params
+    params.require(:donation).permit(:date, :donor_id, :method, :account, :amount, :tax_deductible, :memo)
+  end
 end
