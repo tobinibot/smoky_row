884d62444ddb5280294db0e7e4c2381a9ced9962
Tobin Juday <tobinibot@gmail.com>
2021-02-14 14:36:26 -0500

    logging

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
     @donors = Donor.all
+
+    logger.info("Chrome env? #{ENV['GOOGLE_CHROME_BIN']}")
+
+    # system('google-chrome')
   end
 end
